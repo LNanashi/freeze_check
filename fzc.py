15c707ce21ef31fb31bc8c6796d11e89d2174d17
N0s3nsE <n0s3nsE.NNS@gmail.com>
2019-08-14 06:28:03 +0900

    v1.0

--- fzc.py
+++ fzc.py
@@ -1,12 +1,41 @@
 import requests
+import argparse
 from bs4 import BeautifulSoup
 
-t = "https://twitter.com/"
 
-r = requests.get(t)
 
-sp = BeautifulSoup(r.text, "html.parser")
-print(sp.title.string)
+twurl = "https://twitter.com/"
 
-#print(r.text)
-print(r.status_code)
+class argus_get():
+   pr = argparse.ArgumentParser()
+   pr.add_argument("-f", "--fname", default="id.txt", help="select id file name")
+   args = pr.parse_args()
+
+def main():
+   argus = argus_get()
+   ag = argus.args
+   
+   f_check(ag.fname)
+
+def f_check(fname):
+   f = open(fname, 'r')
+   uid = f.readline()
+   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+   while uid:
+      r = requests.get(twurl + uid.strip(), headers=headers)
+      
+      sp = BeautifulSoup(r.text, "html.parser")
+      
+      if len(r.text) < 300000:
+         print("###@" + uid.strip() + ": freeze or not exist account!!!")
+      
+      else:
+         n = (sp.title.string).find(')') + 1
+         print(sp.title.string[0:n])
+
+      uid = f.readline()
+
+   f.close()
+
+if __name__ == "__main__":
+      main()
